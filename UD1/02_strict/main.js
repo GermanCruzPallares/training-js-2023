cd6417fb2b09139b34ee147a6902b0bbf5de471b
GermanCruzPallares <gcp0007@alu.medac.es>
2023-11-22 14:26:49 +0100

    n

--- UD1/02_strict/main.js
+++ UD1/02_strict/main.js
@@ -1,3 +1,6 @@
+'use scrict'
+
 x = 10
 
 console.log('el valor de X ' + x)
+console.log(`x = ${x}`)
--- UD1/06_math_array/main.js
+++ UD1/06_math_array/main.js
@@ -7,12 +7,25 @@
 
 
 function doCalculation(array) {
+let sum=0
+let numMa=array[0]
+let numMe=array[0]
+for (let index = 0; index < array.length; index++) {
+    
+    sum= sum + array[index]
+    
+    if(array[index]>numMa){
 
-
+       numMa = array[index] 
+    
+    }
 }
 
-
+console.log(sum,numMa)
+}
 
 doCalculation([1,2,3,4])
 doCalculation([5,5,5,5])
 doCalculation([1,1,1,2,2,2,3,3,3,4,4,4,5,5,5])
+
+
